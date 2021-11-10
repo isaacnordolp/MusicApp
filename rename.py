b3a93469abdc973c3605274e6e3baee5e0285471
nicopardom <94014352+nicopardom@users.noreply.github.com>
2021-11-09 20:16:59 -0500

    Guitar Sounds Added

--- rename.py
+++ rename.py
@@ -1,16 +1,16 @@
-import os
+#import os
 
-names =['C','C#','D','D#','E','F','F#','G','G#','A','A#','B']
-duration =['whole','half','quarter','eigth']
-for k in range(4):
-	for i in range(7):
-		for j in range(12):
-			numero = (12*i) + j
-			numeroArchivo = f"{(2*numero) + 1:03d}"
-			archivo = duration[k]+"/Mi canción_"+numeroArchivo+'.wav'
+#names =['C','C#','D','D#','E','F','F#','G','G#','A','A#','B']
+#duration =['whole','half','quarter','eigth']
+#for k in range(4):
+#	for i in range(7):
+		#for j in range(12):
+			#numero = (12*i) + j
+			#numeroArchivo = f"{(2*numero) + 1:03d}"
+			#archivo = duration[k]+"/Mi canción_"+numeroArchivo+'.wav'
 			#print(archivo)
-			nombre = names[j]+str(i)+".wav"
+			#nombre = names[j]+str(i)+".wav"
 			#print(nombre)
-			os.rename(archivo, nombre)
+			#os.rename(archivo, nombre)
 
 	
